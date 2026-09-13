897362f17127a82581f88eb15f934f1aaeb02b97
Moneer Shoukri <128552943+Moneer-S@users.noreply.github.com>
2026-09-13 02:55:42 -0500

    Classify the reused documentation flame in the rights inventory

--- tests_platform/test_release_rights_audit.py
+++ tests_platform/test_release_rights_audit.py
@@ -33,7 +33,7 @@ def test_release_rights_audit_covers_the_release_tree() -> None:
         "frontend_locked_packages": 391,
         "rust_release_crates": 42,
         "rust_locked_crates": 48,
-        "classified_assets": 45,
+        "classified_assets": 46,
         "project_source_files": report.project_source_files,
         "unresolved_items": [],
     }
